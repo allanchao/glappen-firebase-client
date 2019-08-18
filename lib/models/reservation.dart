c894376146fa7bb198a98212a1c46fbfe74ceba7
Lars-Kristian Dahl <lars.dahl@gmail.com>
2019-08-18 02:38:17 +0200

    Update reservation data

--- lib/models/reservation.dart
+++ lib/models/reservation.dart
@@ -18,8 +18,9 @@ class ReservationRef extends Document {
   static const jsonSection = 'section';
   static const jsonUser = 'user';
   static const jsonUserName = 'userName';
-  static const jsonPayment = 'payment';
   static const jsonState = 'state';
+  static const jsonColor = 'color';
+  static const jsonPaymentState = 'paymentStatus';
 
   static Reservation fromFirestore(DocumentSnapshot ds) {
     final data = ds.data;
@@ -38,7 +39,8 @@ class ReservationRef extends Document {
         user: data[ReservationRef.jsonUser],
         userName: data[ReservationRef.jsonUserName],
         hangerName: data[ReservationRef.jsonHangerName],
-        payment: data[ReservationRef.jsonPayment]);
+        color: data[ReservationRef.jsonColor],
+        paymentState: PaymentState.values[data[ReservationRef.jsonPaymentState] + 2]);
   }
 }
 
@@ -49,32 +51,36 @@ class Reservation {
   final String userName;
   final String venueName;
   final String wardrobeName;
+  final String color;
   final Timestamp checkIn;
   final Timestamp checkOut;
   final Timestamp reservationTime;
   final Timestamp stateUpdated;
   final ReservationState state;
+  final PaymentState paymentState;
   final DocumentReference hanger;
   final DocumentReference section;
   final DocumentReference user;
-  final DocumentReference payment;
 
-  Reservation(
-      {@required this.ref,
-      @required this.docId,
-      @required this.checkIn,
-      @required this.checkOut,
-      @required this.reservationTime,
-      @required this.hanger,
-      @required this.hangerName,
-      @required this.section,
-      @required this.userName,
-      @required this.user,
-      @required this.state,
-      @required this.venueName,
-      @required this.wardrobeName,
-      @required this.stateUpdated,
-      @required this.payment});
+  Reservation({
+    @required this.ref,
+    @required this.color,
+    @required this.paymentState,
+    @required this.docId,
+    @required this.checkIn,
+    @required this.checkOut,
+    @required this.reservationTime,
+    @required this.hanger,
+    @required this.hangerName,
+    @required this.section,
+    @required this.userName,
+    @required this.user,
+    @required this.state,
+    @required this.venueName,
+    @required this.wardrobeName,
+    @required this.stateUpdated,
+  });
 }
 
 enum ReservationState { CHECK_IN_REJECTED, CHECKED_OUT, CHECKED_IN, CHECKING_OUT, CHECKING_IN }
+enum PaymentState { REFUNDED, CANCELED, INITIAL, RESERVED, CAPTURED }
